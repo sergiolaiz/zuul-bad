fd7db556833530da9e6824f079c2c0b693698336
unknown <sergio.laiz87@gmail.com>
2019-06-09 13:22:47 +0200

    commit r01

--- Item.java
+++ Item.java
@@ -1,4 +1,3 @@
-
 /**
  * Write a description of class Item here.
  *
@@ -8,19 +7,39 @@
 public class Item
 {
     // instance variables - replace the example below with your own
-    private String nombre;
     private String descripcion;
-
+    private int pesoGramos;
+    private String id;
+    private boolean canBePickedUp ; 
     /**
      * Constructor for objects of class Item
      */
-    public Item(String nombre,String descripcion)
+    public Item(String id,String descripcion, int pesoGramos, boolean canBePickedUp )
     {
-        this.nombre = nombre;
+        this.pesoGramos = pesoGramos;
         this.descripcion = descripcion;
+        this.id = id;
+        this.canBePickedUp  = canBePickedUp ;
+    }
+    
+    
+    public boolean getCanBePickedUp(){
+        return canBePickedUp;
+    }
+    
+    public int getPeso(){
+        return pesoGramos;
+    }
+    
+    public String getDescripcion(){
+        return descripcion;
+    }
+    
+    public String getId(){
+        return id;
     }
 
     public String toString(){
-        return nombre + " :" +descripcion;
+        return id + ": " +descripcion + ", peso " + pesoGramos + "g.";
     }
 }
---
+++ Player.java
@@ -0,0 +1,163 @@
+import java.util.Stack;
+import java.util.ArrayList; 
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    private Room currentRoom;
+    private Stack movimientos;
+    private static final int PESO_MAXIMO_MOCHILA = 400;
+    private ArrayList <Item> mochila;
+    private int pesoMochila;
+    private String nombreJugador;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player(String nombreJugador)
+    {
+        this.nombreJugador =  nombreJugador;
+        movimientos = new  Stack();
+        mochila = new ArrayList();
+        pesoMochila = 0;
+        currentRoom =  null;
+    }
+
+    public Room getRoom(){
+        return currentRoom;
+    }
+
+    public void preguntarUbicacion(){
+        System.out.println(currentRoom.getLongDescription());
+        System.out.println();
+    }
+
+    public void look() {   
+        System.out.println(currentRoom.getItem());
+        preguntarUbicacion();
+    }
+
+    public void setHabitacion(Room habitacion){
+        currentRoom =  habitacion;
+    }
+
+    public void eat() {    
+        System.out.println("Acabas de comer y ya no tienes hambre");
+    }
+
+    public void take (String idItem){
+        String cadeneaADevolver ="";
+        String espacioDisponible = "";
+        Item itemTemp = currentRoom.getItemPorId(idItem);
+        int pesoConObjeto = pesoMochila + itemTemp.getPeso();
+        if(pesoConObjeto <= PESO_MAXIMO_MOCHILA && itemTemp.getCanBePickedUp() ){
+                mochila.add(itemTemp);
+                //Eliminamos el item de la habitacion
+                currentRoom.eliminarItem(idItem);
+                espacioDisponible = String.valueOf(PESO_MAXIMO_MOCHILA - pesoConObjeto) + "g.";
+                pesoMochila += itemTemp.getPeso();
+                cadeneaADevolver = "Has cojido " + itemTemp.getId() +
+                ". Espacio disponible "+ espacioDisponible +"\n";
+        }
+        else if(!itemTemp.getCanBePickedUp()){
+            cadeneaADevolver = "No se puede coger este objeto\n";
+        }
+        else {
+            cadeneaADevolver = "No puedes llevar mas\n";
+        }
+        System.out.println(cadeneaADevolver);
+        preguntarUbicacion();
+    }
+
+    public void guardarHabitacion(Room habitacion ){
+        movimientos.push(habitacion);
+    }
+
+    public void verMochila(){ 
+        if(!mochila.isEmpty()){
+            String cadenaADevolver = "Contenido mochila : \n";
+            for(Item itemTem : mochila){
+                cadenaADevolver += itemTem.getId() +":" + itemTem.getDescripcion() + "\n";
+            }
+            System.out.println(cadenaADevolver + "Espacio disponible :" +(PESO_MAXIMO_MOCHILA - pesoMochila ) + "\n");
+        }
+        else{
+            System.out.println("La mochila esta vacia\n");
+        }
+        preguntarUbicacion();
+    }
+
+    public void dropElemento(String id){
+        if(!mochila.isEmpty()){
+            boolean buscando = true;
+            int cont = 0;
+            while (cont < mochila.size() && buscando ){
+                if(mochila.get(cont).getId().equalsIgnoreCase(id) ){
+                    Item itemTemp = mochila.get(cont);
+                    currentRoom.soltarObjeto(itemTemp);
+                    System.out.println("Has soltado " + itemTemp.getId() + "\n");
+                    pesoMochila -= itemTemp.getPeso();
+                    mochila.remove(cont);
+
+                    buscando =  false;
+                }
+                cont ++;
+            }
+        }
+        preguntarUbicacion();
+    }
+    
+
+
+    /** 
+     * Try to go in one direction. If there is an exit, enter
+     * the new room, otherwise print an error message.
+     */
+    public void goRoom(Command command) 
+    {
+        if(!command.hasSecondWord()) {
+            // if there is no second word, we don't know where to go...
+            System.out.println("¿Donde quieres ir?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        // Try to leave current room.
+        Room nextRoom = currentRoom.getExit(direction);
+
+        if (nextRoom == null) {
+            System.out.println("¡No hay niguna puerta!");
+        }
+        else {
+            //en caso de que la salida exista, la almacenamos en nuestros movimientos para poder
+            // volver atras en caso de que lo necesitemos.
+            Room habitacionActual = currentRoom;
+            guardarHabitacion(habitacionActual);
+            //se almacena la direccion que se sigue.
+            currentRoom = nextRoom;
+            setHabitacion(nextRoom);
+            preguntarUbicacion();
+        }
+    }
+    
+    public void back(){
+        //comprobamos que nuestra lista de movimientos no este vacia para saber si ya regresamos al
+        //inicio
+        if(!movimientos.empty()){
+            //creamos un String con la direccion contraria a la anterior.
+            currentRoom = (Room) movimientos.pop();
+            preguntarUbicacion();
+
+        }
+        else{
+            System.out.println("¡Ya volviste donde empezaste!");
+            preguntarUbicacion();
+        }
+
+    }
+}
